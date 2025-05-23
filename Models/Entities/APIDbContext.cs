3fef34b7aa4a5517fe56c6f31339b40a3755652a
pareshcodz <paresh@codzgarage.com>
2025-05-23 17:47:45 +0530

    project setup

---
+++ Models/Entities/APIDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EventCalender.Models.Entities
+{
+    public class APIDbContext : DbContext
+    {
+        public APIDbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<User> user { get; set; }
+    }
+}
---
+++ Models/Entities/User.cs
@@ -0,0 +1,12 @@
+﻿namespace EventCalender.Models.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? Email { get; set; }
+        public string? Address { get; set; }
+    }
+}
