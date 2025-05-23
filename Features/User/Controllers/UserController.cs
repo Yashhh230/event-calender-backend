7ed229ed2f39909cddfb3c8723dbd62ebd4297a0
pareshcodz <paresh@codzgarage.com>
2025-05-23 18:54:11 +0530

    crud operation completed

---
+++ Features/User/Controllers/UserController.cs
@@ -0,0 +1,122 @@
+﻿using EventCalender.Features.User.DTOs;
+using EventCalender.Models.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventCalender.Features.User.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : Controller
+    {
+
+        private readonly APIDbContext dbContext;
+
+        public UserController(APIDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet("getUserList")]
+        public IActionResult getUser()
+        {
+            return Ok(dbContext.user.ToList());
+
+        }
+
+        [HttpPost("saveUser")]
+        public async Task<IActionResult> addUser(AddUserDTO addUserDTO)
+        {
+            try
+            {
+                var userData = new Models.Entities.User()
+                {
+
+                    FirstName = addUserDTO.FirstName,
+                    LastName = addUserDTO.LastName,
+                    Address = addUserDTO.Address,
+                    Email = addUserDTO.Email,
+                    PhoneNumber = addUserDTO.PhoneNumber,
+                };
+
+                await dbContext.user.AddAsync(userData);
+                await dbContext.SaveChangesAsync();
+                return Ok("");
+
+            }
+            catch(Exception e)
+            {
+                throw e;
+            }
+            //return Ok(dbContext.user.ToList());
+        }
+
+        [HttpGet("getUserById")]
+        public async Task<IActionResult> getUserById([FromQuery] int id)
+        {
+            try
+            {
+                var userId = await dbContext.user.FindAsync(id);
+
+                 if(userId == null)
+                {
+                    return NotFound();
+                }
+                return Ok(userId);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            //return Ok(dbContext.user.ToList());
+        }
+
+        [HttpDelete("deleteUser")]
+        public async Task<IActionResult> deleteUser([FromQuery] int id)
+        {
+            try
+            {
+                var userId = await dbContext.user.FindAsync(id);
+
+                if (userId != null)
+                {
+                     dbContext.Remove(userId);
+                     dbContext.SaveChanges();
+                    return Ok();
+                }
+                return NotFound();
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            //return Ok(dbContext.user.ToList());
+        }
+        [HttpPut("updateUser")]
+        public async Task<IActionResult> updateUser([FromQuery] int id , AddUserDTO addUserDTO)
+        {
+            try
+            {
+                var userId = await dbContext.user.FindAsync(id);
+
+                if (userId != null)
+                {
+                    userId.FirstName = addUserDTO.FirstName;
+                    userId.LastName = addUserDTO.LastName;
+                    userId.PhoneNumber = addUserDTO.PhoneNumber;
+                    userId.Email = addUserDTO.Email;
+                    userId.Address = addUserDTO.Address;
+
+                    
+                }
+                dbContext.SaveChanges();
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            //return Ok(dbContext.user.ToList());
+        }
+    }
+}
---
+++ Features/User/DTOs/AddUserDTO.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EventCalender.Features.User.DTOs
+{
+    public class AddUserDTO
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? PhoneNumber { get; set; }
+
+        [EmailAddress]
+        public string  Email { get; set; }
+        public string? Address { get; set; }
+    }
+}
